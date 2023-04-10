021742fc734ac7598564af6a91f4674d2cb21bc2
Alan Zhan <ayz975@gmail.com>
2023-04-09 23:09:50 -0400

    a

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import UserController from "./controllers/users/users-controller.js";
 import HelloController from "./controllers/hello-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb+srv://ayz975:<ilovecars>@cluster0.bac1ven.mongodb.net/?retryWrites=true&w=majority'
+import mongoose from "mongoose";
+const CONNECTION_STRING =  "mongodb+srv://ayz975:<password>@cluster0.bac1ven.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
